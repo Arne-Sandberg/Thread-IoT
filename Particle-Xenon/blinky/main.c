e11388b0028f97e6f65ac1ec110001520cc698d2
Dillon <ee.tinkerer@gmail.com>
2019-01-29 10:51:04 -0500

    blink LEDs using port and pin instead of board details

--- Particle-Xenon/blinky/main.c
+++ Particle-Xenon/blinky/main.c
@@ -51,24 +51,24 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include "nrf_delay.h"
-#include "boards.h"
+#include "nrf_gpio.h"
 
 /**
  * @brief Function for application main entry.
  */
 int main(void)
 {
-    /* Configure board. */
-    bsp_board_init(BSP_INIT_LEDS);
-
     /* Toggle LEDs. */
     while (true)
     {
-        for (int i = 0; i < LEDS_NUMBER; i++)
-        {
-            bsp_board_led_invert(i);
-            nrf_delay_ms(500);
-        }
+        // D0 is SDA, Port 0, Pin 26
+        // D1 is SCL, Port 0, Pin 27
+
+        nrf_gpio_pin_set(NRF_GPIO_PIN_MAP(0, 26));
+        nrf_delay_ms(500);
+
+        nrf_gpio_pin_clear(NRF_GPIO_PIN_MAP(0, 26));
+        nrf_delay_ms(500);
     }
 }
 
